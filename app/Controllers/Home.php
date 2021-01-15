8e29d5795c05254ae43e61357c9a43ca975a54d2
AlejandroE-Ventura <nekrocerdoslipknotalex@gmail.com>
2021-01-15 00:09:33 -0600

    version

---
+++ app/Controllers/Home.php
@@ -0,0 +1,57 @@
+<?php namespace App\Controllers;
+
+use App\Models\HomePostModel;
+
+class Home extends BaseController
+{
+	public function index(){
+		return view('inicio');
+	}
+	
+	public function obtenerTodosLosMuebles(){
+		$model =  new HomePostModel();
+		$datos = $model->obtenerTodosLosMueblesModel();
+
+		echo json_encode($datos);
+	}
+
+	public function agregarMuebles() {
+
+		$data = array("nombre" => $_POST['mueble'],
+			"tipo" => $_POST['tipo'],
+			"costov" => $_POST['costov'],
+			"costoc" => $_POST['costoc'],
+			"fecha" => $_POST['fecha'],
+		);
+		$model =  new HomePostModel();
+		echo $exito = $model->agregarMueble($data);
+	}
+
+	
+	public function eliminaMueble(){
+		$model =  new HomePostModel();
+		$datos = array("id_mueble" => $_POST['idMueble']);
+		echo $model->eliminaMueble($datos);
+
+	}
+
+	public function obtenerDatosUpdate() {
+		$model =  new HomePostModel();
+		$data = array('id_mueble' => $_POST['idMueble']);
+		echo $model->obtenerDatosUpdate($data);
+	}
+
+	public function actualizarMueble() {
+		$model =  new HomePostModel();
+		$data = array('id_mueble' => $_POST['idMueble'],
+			'nombre' => $_POST['muebleu'],
+			'tipo' => $_POST['tipou'],
+			'costov' => $_POST['costovu'],
+			'costoc' => $_POST['costocu'],
+			'fecha' => $_POST['fechau']
+		);
+		echo $model->actualizarMueble($data);
+	}	
+	//--------------------------------------------------------------------
+
+}
